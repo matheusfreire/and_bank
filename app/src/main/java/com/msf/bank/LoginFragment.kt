9a8026d635cb7d0c7fbb865eb293e5088b884608
matheusfreire <matheus.sfreire@gmail.com>
2019-05-31 14:13:40 -0300

    Create navigation

--- app/src/main/java/com/msf/bank/LoginFragment.kt
+++ app/src/main/java/com/msf/bank/LoginFragment.kt
@@ -6,25 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.msf.bank.databinding.FragmentLoginBinding
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class LoginFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    private lateinit var dataBinding: FragmentLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        dataBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+        return dataBinding.root
     }
 
 
